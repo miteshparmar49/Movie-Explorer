24a0627405c1067c496915b7c8f89854b4424e46
mitesh4949 <mitesh@ganatrainfo.com>
2026-05-10 22:11:59 +0530

    ms clirity

--- app/layout.tsx
+++ app/layout.tsx
@@ -141,6 +141,13 @@ export default function RootLayout({
             gtag('config', 'G-726CX13L75');
           `}
         </Script>
+        <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){`
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    `})(window, document, "clarity", "script", "wp0egv7hlz");
+</script>
         <Header />
         <main>{children}</main>
         <OfferPopup />
