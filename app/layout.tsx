a2225d6edb20c2cc28d2a2a01b25002bf5a977bc
mitesh4949 <mitesh@ganatrainfo.com>
2026-05-10 22:14:19 +0530

    ms clirity u

--- app/layout.tsx
+++ app/layout.tsx
@@ -141,13 +141,17 @@ export default function RootLayout({
             gtag('config', 'G-726CX13L75');
           `}
         </Script>
-        <script type="text/javascript">
-    (function(c,l,a,r,i,t,y){`
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    `})(window, document, "clarity", "script", "wp0egv7hlz");
-</script>
+    
+
+ <Script id="ms-clarity" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "wp0egv7hlz");
+        `}
+      </Script>
         <Header />
         <main>{children}</main>
         <OfferPopup />
